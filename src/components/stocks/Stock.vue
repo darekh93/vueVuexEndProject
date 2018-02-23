1f0672e86ee8adca7e7d6deaefbfe4b2b4dca682
Dariusz Hebda <darekhebda@gmail.com>
2018-02-23 15:15:11 +0100

    Add portfolio

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -14,19 +14,26 @@
                         class="form-control"
                         placeholder="Quantity"
                         v-model="quantity"
+                        :class="{danger: insufficientFunds}"
                         >
                 </div>
                 <div class="pull-right">
                     <button
                         class="btn btn-success"
                         @click="buyStock"
-                        :disabled="quantity <= 0 || !Number.isInteger(quantity)"
-                        >Buy</button>
+                        :disabled="insufficientFunds || quantity <= 0 || Number.isInteger(quantity)"
+                        >{{ insufficientFunds ? 'insufficientFunds' : 'Buy' }}</button>
                 </div>
             </div>
         </div>
     </div>
 </template>
+
+<style>
+    .danger {
+        border: 1px solid red;
+    }
+</style>
 
 <script>
     export default {
@@ -36,6 +43,14 @@
                 quantity: 0
             }
         },
+        computed: {
+            funds() {
+                return this.$store.getters.funds;
+            },
+            insufficientFunds() {
+                return this.quantity * this.stock.price > this.funds;
+            }
+        },
         methods: {
             buyStock() {
                 const order = {
@@ -43,7 +58,7 @@
                     stockPrice: this.stock.price,
                     quantity: this.quantity
                 };
-                console.log(order);
+                this.$store.dispatch('buyStock', order)
                 this.quantity = 0;
             }
         }
