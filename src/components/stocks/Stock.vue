a9114222421623d9c9778b171eacde4f778413c5
Dariusz Hebda <darekhebda@gmail.com>
2018-02-27 15:08:27 +0100

    create animations and rest with firebase

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -56,7 +56,7 @@
                 const order = {
                     stockId: this.stock.id,
                     stockPrice: this.stock.price,
-                    quantity: this.quantity
+                    quantity: parseInt(this.quantity)
                 };
                 this.$store.dispatch('buyStock', order)
                 this.quantity = 0;
